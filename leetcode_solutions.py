bd2ae1a86e396d1b4b01dae392fb7d82e66a0ca4
pannaga3009 <pannaga3009@gmail.com>
2024-04-02 15:36:28 -0400

    added leetcode question 3

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -454,6 +454,37 @@ Explanation: Swap s1[0] and s2[1], s1 = "yx", s2 = "yx".
 
 print("Minimum swap required ", minimumSwap("xx", "yy")) 
 
+def lengthOfLongestSubstring(s):
+    """
+    Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+    """
+    char_set = set()
+    #Have a set to store all the characters visited
+    max_length = 0
+    left = right = 0
+
+    while left < len(s) and right < len(s):
+        if s[right] not in char_set:
+            char_set.add(s[right])
+            right += 1
+            max_length = max(max_length, right - left)
+        else:
+            char_set.remove(s[left])
+            left += 1
+    
+    return max_length
+
+print("Length of the longest substring is ", lengthOfLongestSubstring("abcabcbb"))
+        
+    
 
 
 
