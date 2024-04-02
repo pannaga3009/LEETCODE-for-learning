1dbe9eaccf5903053c8f9ae1a638ff8a511292ae
pannaga3009 <pannaga3009@gmail.com>
2024-04-02 15:15:00 -0400

    added 1247

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -425,7 +425,35 @@ Output: "2052-10-20"
 date = "20th Oct 2052"
 print(f" Date is {reformatDate(date)}")
 
+def minimumSwap(s1, s2):
+    """
+You are given two strings s1 and s2 of equal length consisting of letters "x" and "y" only. Your task is to make these two strings equal to each other. You can swap any two characters that belong to different strings, which means: swap s1[i] and s2[j].
+Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
+Example 1:
+
+Input: s1 = "xx", s2 = "yy"
+Output: 1
+Explanation: Swap s1[0] and s2[1], s1 = "yx", s2 = "yx".
+    """
+    count_x = 0
+    count_y = 0
+
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if c1 == "x":
+                count_x += 1
+            else:
+                count_y += 1
+        
     
+    if (count_x + count_y) % 2 != 0:
+        return -1 
+
+    return (count_x + 1 // 2) + (count_y + 1 // 2)
+
+
+print("Minimum swap required ", minimumSwap("xx", "yy")) 
+
 
 
 
