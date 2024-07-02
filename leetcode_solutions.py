d5f5d05aa6cb4bcbe2676fabf5a63485dc18aed8
pannaga3009 <pannaga3009@gmail.com>
2024-07-02 12:00:59 -0400

    leetcode 36

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -682,7 +682,35 @@ nums = [4,5,6,7,0,1,2]
 target = 1
 print(leetcode33(nums, target))
 
-    
+def leetcode36(board):
+    """
+    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ 
+    """
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+
+            box_index = (r // 3) * 3 + (c // 3)
+            if (board[r][c] in rows[r] or board[r][c] in cols[c] 
+                or board[r][c] in boxes[box_index]):
+                return False
+            rows[r].add(board[r][c])
+            cols[c].add(board[r][c])
+            boxes[box_index].add(board[r][c])
+    return True
+
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+print("Executed Sudoku", leetcode36(board))
 
 
 
