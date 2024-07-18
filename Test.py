52a2a9542a68a8ee040c0618860f23ef39f3acc6
pannaga3009 <pannaga3009@gmail.com>
2024-07-18 16:03:07 -0400

    LinkedIn for Learning

--- Test.py
+++ Test.py
@@ -530,3 +530,57 @@ k = 20
 nums = [3,3,3,3]
 k2 = 6
 print("Minimum length of the compressed array is: ", compressedArray(nums, k2))
+
+def HouseRobber(nums):
+    dp = [0] * len(nums)
+
+
+    if len(nums) == 1:
+        return nums[0]
+    if len(nums) == 2:
+        return max(nums[0], nums[1])
+    else:
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+
+    return dp[-1]
+
+nums = [1,2,3,1]
+print("House Robber -- ", HouseRobber(nums))
+
+def HouseRobber2(nums):
+    """
+    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+    Example 1:
+
+    Input: nums = [2,3,2]
+    Output: 3
+    Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+    """
+    if len(nums) == 0:
+        return
+    elif len(nums) == 1:
+        return nums[0]
+    elif len(nums) == 2:
+        return max(nums[0], nums[1])
+    
+    def finding(houses):
+        if len(houses) == 0:
+                return 0
+        elif len(houses) == 1:
+            return houses[0]
+        dp = [0] * len(houses)
+        dp[0] = houses[0]
+        dp[1] = max(houses[0], houses[1])
+        for i in range(2, len(houses)):
+            dp[i] = max(houses[i] + dp[i-2], dp[i-1])
+        return dp[-1]
+
+    return max(finding(nums[:-1]), finding(nums[1:]))
+
+nums = [2,3,2]
+print(" House Robber 2 --- ", HouseRobber2(nums))
