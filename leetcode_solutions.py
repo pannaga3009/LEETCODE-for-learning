c8beda78f72c12112babb5b0688c2ccc1d35c9a7
pannaga3009 <pannaga3009@gmail.com>
2024-07-25 18:11:33 -0400

    Valid graph problem

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -900,6 +900,47 @@ def leetcode322(tickets):
 
 tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
 print("Reconnect itinerary ",leetcode322(tickets))
+
+def leetcode261(n, edges):
+    """
+    You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+    Return true if the edges of the given graph make up a valid tree, and false otherwise.
+    n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+    """
+
+    if not n:
+        return True
+
+    adj = { i: [] for i in range(n)}
+
+    for n1, n2 in edges:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
+
+    visited = set()
+    def dfs(i, prev):
+        if i in visited:
+            return False
+        
+        visited.add(i)
+        for j in adj[i]:
+            if j == prev:
+                continue
+
+            if not dfs(j, i):
+                return False
+        return True
+
+    return dfs(0, -1) and len(visited) == n
+
+edges = [[0,1],[0,2],[0,3],[1,4]]
+n = 5
+print("Graph Valid Tree - ", leetcode261(n,edges))
+
+
+    
+
+    
         
 
 
