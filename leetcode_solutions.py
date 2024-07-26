2dd0d25c7b4a6460032b1beef305da18a6997159
pannaga3009 <pannaga3009@gmail.com>
2024-07-26 12:51:22 -0400

    leetcode 875

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -937,11 +937,34 @@ edges = [[0,1],[0,2],[0,3],[1,4]]
 n = 5
 print("Graph Valid Tree - ", leetcode261(n,edges))
 
-
+import math
+def minEatingSpeed(piles, h):
+    """
+    Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+    Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+    Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+    Return the minimum integer k such that she can eat all the bananas within h hours.
+    """
+    left, right = 1, max(piles)
+    res = right
     
+    while left <= right:
+        hours = 0
+        mid = (left + right) // 2
+        for p in piles:
+            hours += math.ceil(p/mid)
 
-    
-        
+        if hours <= h:
+            res = min(res, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return res
+
+piles = [3,6,7,11]
+h = 8
+print("Koko eating min bananas  ", minEatingSpeed(piles,h))
 
 
 
